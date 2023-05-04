912b046d0c409d624d7fe3d3e1843ec059712835
cdmi123 <languagepdf@gmail.com>
2023-05-04 13:20:04 +0530

    msg

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ localStorage = new LocalStorage('./scratch');
 
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'http://localhost:5000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
